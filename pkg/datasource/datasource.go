7a823671f8387aa946d15890f204937f8721f8a4
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-07-02 13:22:43 +0200

    Fix lint error

--- pkg/datasource/datasource.go
+++ pkg/datasource/datasource.go
@@ -1,13 +1,14 @@
 package datasource
 
 import (
-  "github.com/grafana/x-ray-datasource/pkg/client"
-  "github.com/grafana/x-ray-datasource/pkg/configuration"
-  "net/http"
+	"net/http"
 
-  "github.com/grafana/grafana-plugin-sdk-go/backend"
-  "github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
-  "github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+	"github.com/grafana/x-ray-datasource/pkg/client"
+	"github.com/grafana/x-ray-datasource/pkg/configuration"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 )
 
 // newDatasource returns datasource.ServeOpts.
@@ -18,7 +19,6 @@ func GetServeOpts() datasource.ServeOpts {
 	ds := NewDatasource(getXrayClient)
 	ds.im = datasource.NewInstanceManager(newDataSourceInstanceSettings)
 
-
 	return datasource.ServeOpts{
 		QueryDataHandler:   ds.QueryMux,
 		CheckHealthHandler: ds,
@@ -26,58 +26,57 @@ func GetServeOpts() datasource.ServeOpts {
 }
 
 type instanceSettings struct {
-  httpClient *http.Client
+	httpClient *http.Client
 }
 
 func newDataSourceInstanceSettings(setting backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-  return &instanceSettings{
-    httpClient: &http.Client{},
-  }, nil
+	return &instanceSettings{
+		httpClient: &http.Client{},
+	}, nil
 }
 
-
 func (s *instanceSettings) Dispose() {
-  // Called before creatinga a new instance to allow plugin authors
-  // to cleanup.
+	// Called before creatinga a new instance to allow plugin authors
+	// to cleanup.
 }
 
 type Datasource struct {
 	// The instance manager can help with lifecycle management
 	// of datasource instances in plugins. It's not a requirements
 	// but a best practice that we recommend that you follow.
-	im instancemgmt.InstanceManager
-	QueryMux *datasource.QueryTypeMux
-	xrayClientFactory func (pluginContext *backend.PluginContext) (XrayClient, error)
+	im                instancemgmt.InstanceManager
+	QueryMux          *datasource.QueryTypeMux
+	xrayClientFactory func(pluginContext *backend.PluginContext) (XrayClient, error)
 }
 
-const(
-  QueryGetTrace string = "getTrace"
-  QueryGetTraceSummaries = "getTraceSummaries"
-  QueryGetTimeSeriesServiceStatistics = "getTimeSeriesServiceStatistics"
+const (
+	QueryGetTrace                       = "getTrace"
+	QueryGetTraceSummaries              = "getTraceSummaries"
+	QueryGetTimeSeriesServiceStatistics = "getTimeSeriesServiceStatistics"
 )
 
-func NewDatasource(xrayClientFactory func (pluginContext *backend.PluginContext) (XrayClient, error)) *Datasource {
-  ds := &Datasource{
-    xrayClientFactory: xrayClientFactory,
-  }
+func NewDatasource(xrayClientFactory func(pluginContext *backend.PluginContext) (XrayClient, error)) *Datasource {
+	ds := &Datasource{
+		xrayClientFactory: xrayClientFactory,
+	}
 
-  mux := datasource.NewQueryTypeMux()
-  mux.HandleFunc(QueryGetTrace, ds.getTrace)
-  mux.HandleFunc(QueryGetTraceSummaries, ds.getTraceSummaries)
-  mux.HandleFunc(QueryGetTimeSeriesServiceStatistics, ds.getTimeSeriesServiceStatistics)
+	mux := datasource.NewQueryTypeMux()
+	mux.HandleFunc(QueryGetTrace, ds.getTrace)
+	mux.HandleFunc(QueryGetTraceSummaries, ds.getTraceSummaries)
+	mux.HandleFunc(QueryGetTimeSeriesServiceStatistics, ds.getTimeSeriesServiceStatistics)
 
-  ds.QueryMux = mux
-  return ds
+	ds.QueryMux = mux
+	return ds
 }
 
 func getXrayClient(pluginContext *backend.PluginContext) (XrayClient, error) {
-  dsInfo, err := configuration.GetDatasourceInfo(pluginContext.DataSourceInstanceSettings, "default")
-  if err != nil {
-    return nil, err
-  }
-  xrayClient, err := client.CreateXrayClient(dsInfo)
-  if err != nil {
-    return nil, err
-  }
-  return xrayClient, nil
+	dsInfo, err := configuration.GetDatasourceInfo(pluginContext.DataSourceInstanceSettings, "default")
+	if err != nil {
+		return nil, err
+	}
+	xrayClient, err := client.CreateXrayClient(dsInfo)
+	if err != nil {
+		return nil, err
+	}
+	return xrayClient, nil
 }
