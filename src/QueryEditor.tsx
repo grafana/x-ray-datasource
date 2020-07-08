ffa4de1c9be5c59cf70e39998e1cba3213c2b8e0
Andrej Ocenas <mr.ocenas@gmail.com>
2020-07-08 12:50:49 +0200

    Update query editor

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,32 +1,108 @@
 import React from 'react';
-import { LegacyForms, Segment } from '@grafana/ui';
-import { QueryEditorProps, SelectableValue } from '@grafana/data';
+import { LegacyForms, Segment, useStyles, InlineFormLabel } from '@grafana/ui';
+import { QueryEditorProps } from '@grafana/data';
+import { css } from 'emotion';
 import { XrayDataSource } from './DataSource';
 import { XrayJsonData, XrayQuery, XrayQueryType } from './types';
+import { useMount } from 'react-use';
 
 const { FormField } = LegacyForms;
 
-type Props = QueryEditorProps<XrayDataSource, XrayQuery, XrayJsonData>;
+function getStyles() {
+  return {
+    FormField: css`
+      flex: 1;
+    `,
+  };
+}
 
+/**
+ * We do some mapping of the actual queryTypes to options user can select. Mainly don't want user to choose
+ * between trace list and single trace and we detect that based on query. So trace list option returns single trace
+ * if query contains single traceID.
+ */
+enum QueryTypeOptions {
+  traceList = 'Trace List',
+  traceAnalytics = 'Trace Analytics',
+}
+
+function queryTypeToQueryTypeOptions(queryType?: XrayQueryType): QueryTypeOptions {
+  if (queryType === XrayQueryType.getTrace || queryType === XrayQueryType.getTraceSummaries) {
+    return QueryTypeOptions.traceList;
+  } else {
+    return QueryTypeOptions.traceAnalytics;
+  }
+}
+
+function queryTypeOptionToQueryType(queryTypeOption: QueryTypeOptions, query: string): XrayQueryType {
+  if (queryTypeOption === QueryTypeOptions.traceList) {
+    const isTraceIdQuery = !!query.trim().match(/^\d-\w{8}-\w{24}$/);
+    return isTraceIdQuery ? XrayQueryType.getTrace : XrayQueryType.getTraceSummaries;
+  } else {
+    return XrayQueryType.getTimeSeriesServiceStatistics;
+  }
+}
+
+type Props = QueryEditorProps<XrayDataSource, XrayQuery, XrayJsonData>;
 export function QueryEditor({ query, onChange }: Props) {
+  useInitQuery(query, onChange);
+  const styles = useStyles(getStyles);
+  const queryTypeOption = queryTypeToQueryTypeOptions(query.queryType);
+
   return (
-    <div className="gf-form">
-      <Segment
-        value={query.queryType}
-        options={Object.values(XrayQueryType).map(s => ({ value: s, label: s } as SelectableValue<XrayQueryType>))}
-        onChange={({ value }) => onChange({ ...query, queryType: value! } as any)}
-      />
+    <div>
+      <div className={'gf-form'}>
+        <InlineFormLabel width={'auto'}>Query Type</InlineFormLabel>
+        <Segment
+          value={queryTypeOption}
+          options={Object.keys(QueryTypeOptions).map(key => ({
+            value: key,
+            // @ts-ignore get get rid of implicit any here
+            label: QueryTypeOptions[key],
+          }))}
+          onChange={({ value }) => {
+            onChange({
+              ...query,
+              queryType: queryTypeOptionToQueryType(
+                // @ts-ignore get get rid of implicit any here
+                QueryTypeOptions[value!],
+                query.query || ''
+              ),
+            } as any);
+          }}
+        />
+      </div>
       <FormField
+        className={styles.FormField}
+        inputWidth={null}
         value={query.query || ''}
-        onChange={e =>
+        onChange={e => {
           onChange({
             ...query,
+            queryType: queryTypeOptionToQueryType(queryTypeOption, e.currentTarget.value),
             query: e.currentTarget.value,
-          })
-        }
-        label="Query Text"
+          });
+        }}
+        label="Query"
         tooltip="Not used yet"
+        data-testid={'query-input'}
       />
     </div>
   );
+}
+
+/**
+ * Inits the query with queryType so the segment component is filled in.
+ */
+function useInitQuery(query: XrayQuery, onChange: (value: XrayQuery) => void) {
+  useMount(() => {
+    // We assume that if there is no queryType during mount there should not be any query so we do not need to
+    // check if query has traceId or not as we do with the QueryTypeOptions mapping.
+    if (!query.queryType) {
+      onChange({
+        ...query,
+        queryType: XrayQueryType.getTraceSummaries,
+      });
+    }
+  });
 }
