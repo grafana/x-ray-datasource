b7458e442b1e72fb2ec957100ac780a16e8fe9c8
Andrej Ocenas <mr.ocenas@gmail.com>
2020-07-21 12:54:20 +0200

    Fix wrong query type on traceID query

--- src/components/QueryEditor.test.tsx
+++ src/components/QueryEditor.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { QueryEditor, queryTypeOptionToQueryType, QueryTypeOptions } from './QueryEditor';
 import { XrayQuery, XrayQueryType } from '../types';
 
@@ -8,6 +8,15 @@ const defaultProps = {
   datasource: undefined as any,
   onRunQuery: undefined as any,
 };
+
+jest.mock('./XRayQueryField', () => {
+  return {
+    __esModule: true,
+    XRayQueryField: jest.fn(props => (
+      <input data-testid={'query-field-mock'} onChange={e => props.onChange({ query: e.target.value })} />
+    )),
+  };
+});
 
 function renderWithQuery(query: Omit<XrayQuery, 'refId'>) {
   render(
@@ -51,6 +60,29 @@ it('inits the query with query type', () => {
     refId: 'A',
     query: '',
     queryType: XrayQueryType.getTraceSummaries,
+  });
+});
+
+it('correctly changes the query type if user fills in trace id', () => {
+  const onChange = jest.fn();
+  render(
+    <QueryEditor
+      {...{
+        ...defaultProps,
+        query: { refId: 'A', query: '', queryType: XrayQueryType.getTraceSummaries },
+        onChange,
+      }}
+    />
+  );
+
+  const field = screen.getByTestId('query-field-mock');
+
+  fireEvent.change(field, { target: { value: '1-5f160a8b-83190adad07f429219c0e259' } });
+
+  expect(onChange.mock.calls[0][0]).toEqual({
+    refId: 'A',
+    query: '1-5f160a8b-83190adad07f429219c0e259',
+    queryType: XrayQueryType.getTrace,
   });
 });
 
