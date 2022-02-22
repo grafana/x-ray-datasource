5758ffd126fd8336b400f5e8a67b707360b3b8b5
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-02-22 18:05:41 +0100

    Leave user-agent from GetSession

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,19 +1,12 @@
 package client
 
 import (
-	"fmt"
-	"os"
-	"runtime"
-
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/xray"
 	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/xray"
 )
 
 // Global session cache
@@ -46,13 +39,7 @@ func CreateXrayClient(datasourceInfo *awsds.AWSDatasourceSettings, backendSettin
 		config.Endpoint = aws.String(datasourceInfo.Endpoint)
 	}
 
-	clt := xray.New(sess, config)
-	clt.Handlers.Send.PushFront(func(r *request.Request) {
-		r.HTTPRequest.Header.Set("User-Agent", userAgentString())
-		log.DefaultLogger.Debug("CreateXrayClient", "userAgent", userAgentString())
-	})
-
-	return clt, nil
+	return xray.New(sess, config), nil
 }
 
 // CreateEc2Client creates client for EC2 api. We need this for some introspection queries like getting regions.
@@ -77,13 +64,5 @@ func CreateEc2Client(awsSettings *awsds.AWSDatasourceSettings, backendSettings *
 		return nil, err
 	}
 
-	ec2Client := ec2.New(sess, &aws.Config{})
-	ec2Client.Handlers.Send.PushFront(func(r *request.Request) {
-		r.HTTPRequest.Header.Set("User-Agent", userAgentString())
-	})
-	return ec2Client, nil
-}
-
-func userAgentString() string {
-	return fmt.Sprintf("%s/%s (%s; %s) Grafana/%s", aws.SDKName, aws.SDKVersion, runtime.Version(), runtime.GOOS, os.Getenv("GF_VERSION"))
+	return ec2.New(sess, &aws.Config{}), nil
 }
