127931363a3bd50142b4b1ca01027935061f14b2
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-02-23 10:25:16 +0100

    Mutualize session and put back user-agent log

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/xray"
 	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
@@ -14,22 +15,7 @@ var sessions = awsds.NewSessionCache()
 
 // CreateXrayClient creates a new session and xray client and sets tracking header on that client
 func CreateXrayClient(datasourceInfo *awsds.AWSDatasourceSettings, backendSettings *backend.DataSourceInstanceSettings) (*xray.XRay, error) {
-	httpClientProvider := httpclient.NewProvider()
-	httpClientOptions, err := backendSettings.HTTPClientOptions()
-	if err != nil {
-		return nil, err
-	}
-	httpClient, err := httpClientProvider.New(httpClientOptions)
-	if err != nil {
-		backend.Logger.Error("failed to create HTTP client", "error", err.Error())
-		return nil, err
-	}
-
-	sess, err := sessions.GetSession(awsds.SessionConfig{
-		Settings:      *datasourceInfo,
-		HTTPClient:    httpClient,
-		UserAgentName: aws.String("X-ray"),
-	})
+	sess, err := getXRaySession(datasourceInfo, backendSettings)
 	if err != nil {
 		return nil, err
 	}
@@ -39,30 +25,37 @@ func CreateXrayClient(datasourceInfo *awsds.AWSDatasourceSettings, backendSettin
 		config.Endpoint = aws.String(datasourceInfo.Endpoint)
 	}
 
+	backend.Logger.Debug("CreateXrayClient", "userAgent", awsds.GetUserAgentString("X-ray"))
+
 	return xray.New(sess, config), nil
 }
 
 // CreateEc2Client creates client for EC2 api. We need this for some introspection queries like getting regions.
-func CreateEc2Client(awsSettings *awsds.AWSDatasourceSettings, backendSettings *backend.DataSourceInstanceSettings) (*ec2.EC2, error) {
+func CreateEc2Client(datasourceInfo *awsds.AWSDatasourceSettings, backendSettings *backend.DataSourceInstanceSettings) (*ec2.EC2, error) {
+	sess, err := getXRaySession(datasourceInfo, backendSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	return ec2.New(sess, &aws.Config{}), nil
+}
+
+func getXRaySession(datasourceInfo *awsds.AWSDatasourceSettings, backendSettings *backend.DataSourceInstanceSettings) (*session.Session, error) {
 	httpClientProvider := httpclient.NewProvider()
 	httpClientOptions, err := backendSettings.HTTPClientOptions()
 	if err != nil {
 		return nil, err
 	}
+
 	httpClient, err := httpClientProvider.New(httpClientOptions)
 	if err != nil {
 		backend.Logger.Error("failed to create HTTP client", "error", err.Error())
 		return nil, err
 	}
 
-	sess, err := sessions.GetSession(awsds.SessionConfig{
-		Settings:      *awsSettings,
+	return sessions.GetSession(awsds.SessionConfig{
+		Settings:      *datasourceInfo,
 		HTTPClient:    httpClient,
 		UserAgentName: aws.String("X-ray"),
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	return ec2.New(sess, &aws.Config{}), nil
 }
