a8e4755841f62c41e03c12263becbf0f3256c2e7
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-11-09 10:50:48 +0100

    Fix backend linter

--- pkg/datasource/getAccounts.go
+++ pkg/datasource/getAccounts.go
@@ -11,66 +11,79 @@ import (
 )
 
 type Account struct {
-  Id string
+	Id string
 }
 
 func (ds *Datasource) GetAccounts(rw http.ResponseWriter, req *http.Request) {
-  if req.Method != "GET" {
-    rw.WriteHeader(http.StatusMethodNotAllowed)
-    return
-  }
+	if req.Method != "GET" {
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
-  pluginConfig := httpadapter.PluginConfigFromContext(req.Context())
+	pluginConfig := httpadapter.PluginConfigFromContext(req.Context())
 	xrayClient, err := ds.xrayClientFactory(&pluginConfig)
 
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 
-  urlQuery, err := url.ParseQuery(req.URL.RawQuery)
+	urlQuery, err := url.ParseQuery(req.URL.RawQuery)
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 	group := urlQuery.Get("group")
 
-  layout := "2006-01-02T15:04:05.000Z"
-  startTime, err := time.Parse(layout, urlQuery.Get("startTime"))
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
-  
-  endTime, err := time.Parse(layout, urlQuery.Get("endTime"))
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
+	layout := "2006-01-02T15:04:05.000Z"
+	startTime, err := time.Parse(layout, urlQuery.Get("startTime"))
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 
-  input := &xray.GetServiceGraphInput{
+	endTime, err := time.Parse(layout, urlQuery.Get("endTime"))
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
+
+	input := &xray.GetServiceGraphInput{
 		StartTime: &startTime,
 		EndTime:   &endTime,
 		GroupName: &group,
 	}
 
-  accounts := []Account{};
+	accounts := []Account{}
 
-  err = xrayClient.GetServiceGraphPagesWithContext(req.Context(), input, func(page *xray.GetServiceGraphOutput, lastPage bool) bool {
+	err = xrayClient.GetServiceGraphPagesWithContext(req.Context(), input, func(page *xray.GetServiceGraphOutput, lastPage bool) bool {
 		for _, service := range page.Services {
-      if service.AccountId != nil {
-        account := Account{
-          Id: *service.AccountId,
-        }
-        accounts = append(accounts, account)
-      }        
+			if service.AccountId != nil {
+				account := Account{
+					Id: *service.AccountId,
+				}
+				accounts = append(accounts, account)
+			}
 		}
+
 		// Not sure how many pages there can possibly be but for now try to iterate over all the pages.
 		return true
 	})
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 
-  body, err := json.Marshal(accounts)
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
+	body, err := json.Marshal(accounts)
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 
-  rw.Header().Set("content-type", "application/json")
-  _, err = rw.Write(body)
+	rw.Header().Set("content-type", "application/json")
+	_, err = rw.Write(body)
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 }
