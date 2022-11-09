1c2569381b015fba2b2e8be4d0a38c0ca354ada0
Isabella Siu <isabella.siu@grafana.com>
2022-11-09 14:26:48 -0500

    update config regions

--- src/components/ConfigEditor/ConfigEditor.tsx
+++ src/components/ConfigEditor/ConfigEditor.tsx
@@ -1,6 +1,7 @@
 import { AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData, ConnectionConfig } from '@grafana/aws-sdk';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import React, { PureComponent } from 'react';
+import { standardRegions } from './regions';
 
 export type Props = DataSourcePluginOptionsEditorProps<AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData>;
 
@@ -8,7 +9,7 @@ export class ConfigEditor extends PureComponent<Props> {
   render() {
     return (
       <div>
-        <ConnectionConfig {...this.props} />
+        <ConnectionConfig {...this.props} standardRegions={standardRegions} />
         {/* can add x-ray specific things here */}
       </div>
     );
