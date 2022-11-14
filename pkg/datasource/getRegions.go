c77f527e6ac7a0b573217725b8c2b71c48e2cf33
Isabella Siu <Isabella.siu@grafana.com>
2022-11-14 11:30:33 -0500

    Remove EC2 region dependency (#150)

--- pkg/datasource/getRegions.go
+++ pkg/datasource/getRegions.go
@@ -1,54 +1,41 @@
 package datasource
 
 import (
-  "encoding/json"
-  "github.com/aws/aws-sdk-go/service/ec2"
-  "net/http"
+	"encoding/json"
+	"net/http"
 
-  "github.com/grafana/grafana-plugin-sdk-go/backend/log"
-  "github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/service/xray"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
 
-func (ds *Datasource) getRegions(rw http.ResponseWriter, req *http.Request) {
-  if req.Method != "GET" {
-    rw.WriteHeader(http.StatusMethodNotAllowed)
-    return
-  }
+func (ds *Datasource) GetRegions(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
-  pluginConfig := httpadapter.PluginConfigFromContext(req.Context())
-  ec2Client, err := ds.ec2ClientFactory(&pluginConfig)
+	regions := []string{}
+	for _, partition := range endpoints.DefaultPartitions() {
+		regionsForPartition, exists := endpoints.RegionsForService(endpoints.DefaultPartitions(), partition.ID(), xray.EndpointsID)
+		if exists {
+			for region := range regionsForPartition {
+				regions = append(regions, region)
+			}
+		}
+	}
 
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
+	body, err := json.Marshal(regions)
+	if err != nil {
+		sendError(rw, err)
+		return
+	}
 
-  regions, err := getRegions(ec2Client)
-
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
-
-  body, err := json.Marshal(regions)
-  if err != nil {
-    sendError(rw, err)
-    return
-  }
-
-  rw.Header().Set("content-type", "application/json")
-  _, err = rw.Write(body)
-  if err != nil {
-    log.DefaultLogger.Error("failed to write response", "err", err.Error())
-    return
-  }
-}
-
-func getRegions(client *ec2.EC2) ([]*ec2.Region, error) {
-  input := &ec2.DescribeRegionsInput{}
-  out, err := client.DescribeRegions(input)
-  if err != nil {
-    return nil, err
-  }
-  return out.Regions, err
+	rw.Header().Set("content-type", "application/json")
+	_, err = rw.Write(body)
+	if err != nil {
+		log.DefaultLogger.Error("failed to write response", "err", err.Error())
+		return
+	}
 }
---
+++ pkg/datasource/getRegions_test.go
@@ -0,0 +1,26 @@
+package datasource_test
+
+import (
+	"io"
+	"testing"
+
+	"net/http/httptest"
+
+	"github.com/grafana/x-ray-datasource/pkg/datasource"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetRegions(t *testing.T) {
+	t.Run("when passed a get request it returns a list of regions from aws from all supported clouds", func(t *testing.T) {
+		ds := datasource.NewDatasource(xrayClientFactory)
+		req := httptest.NewRequest("GET", "http://example.com/regions", nil)
+		w := httptest.NewRecorder()
+		ds.GetRegions(w, req)
+		resp := w.Result()
+		body, _ := io.ReadAll(resp.Body)
+		regionsList := string(body)
+		require.Contains(t, regionsList, "us-east-1")
+		require.Contains(t, regionsList, "us-gov-east-1")
+		require.Contains(t, regionsList, "cn-north-1")
+	})
+}
