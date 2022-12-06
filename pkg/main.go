0222e5845349303227b81187fedb80ffeb426bcd
Erik Sundell <erik.sundell87@gmail.com>
2022-12-06 14:17:20 +0100

    add spellcheck

--- pkg/main.go
+++ pkg/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Start listening to requests send from Grafana. This call is blocking so
-	// it won't finish until Grafana shutsdown the process or the plugin choose
+	// it won't finish until Grafana shuts down the process or the plugin choose
 	// to exit close down by itself
 	err := datasource.Serve(xraydatasource.GetServeOpts())
 
