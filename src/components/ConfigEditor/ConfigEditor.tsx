9568dd81cd38dbd75d952c7629c9e9eceab8306f
Kevin Yu <kevinwcyu@users.noreply.github.com>
2024-12-20 13:14:39 -0800

    Add PDC support

--- src/components/ConfigEditor/ConfigEditor.tsx
+++ src/components/ConfigEditor/ConfigEditor.tsx
@@ -1,6 +1,9 @@
 import { AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData, ConnectionConfig } from '@grafana/aws-sdk';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { config } from '@grafana/runtime';
+import { SecureSocksProxySettings } from '@grafana/ui';
 import React from 'react';
+import { gte } from 'semver';
 import { standardRegions } from './regions';
 
 export type Props = DataSourcePluginOptionsEditorProps<AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData>;
@@ -9,6 +12,9 @@ export function ConfigEditor(props: Props) {
   return (
     <div className="width-30">
       <ConnectionConfig {...props} standardRegions={standardRegions} />
+      {config.secureSocksDSProxyEnabled && gte(config.buildInfo.version, '10.0.0') && (
+        <SecureSocksProxySettings options={props.options} onOptionsChange={props.onOptionsChange} />
+      )}
     </div>
   );
 }
