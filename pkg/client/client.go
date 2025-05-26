1acd41e1c413ca3bb9326ed4d21ed3d359d80669
Nathan Vērzemnieks <njvrzm@gmail.com>
2025-05-26 04:11:44 -0700

    Update for grafana-aws-sdk new style v2 authentication  (#336)

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,75 +1,73 @@
 package client
 
 import (
-	"context"
+  "context"
+  "github.com/grafana/grafana-aws-sdk/pkg/awsauth"
+  "net/http"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/applicationsignals"
-	"github.com/aws/aws-sdk-go-v2/service/xray"
+  "github.com/aws/aws-sdk-go-v2/aws"
+  "github.com/aws/aws-sdk-go-v2/service/applicationsignals"
+  "github.com/aws/aws-sdk-go-v2/service/xray"
 
-	//"github.com/aws/aws-sdk-go-v2/session"
-	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
-	"github.com/grafana/grafana-plugin-sdk-go/experimental/errorsource"
+  //"github.com/aws/aws-sdk-go-v2/session"
+  "github.com/grafana/grafana-aws-sdk/pkg/awsds"
+  "github.com/grafana/grafana-plugin-sdk-go/backend"
+  "github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
+  "github.com/grafana/grafana-plugin-sdk-go/experimental/errorsource"
 )
 
 // CreateXrayClient creates a new session and xray client and sets tracking header on that client
-func CreateXrayClient(ctx context.Context, datasourceInfo awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings, sessions *awsds.SessionCache) (*xray.Client, error) {
-	provider, err := getXRaySession(ctx, datasourceInfo, backendSettings, sessions)
-	if err != nil {
-		return nil, err
-	}
-
-	config := aws.Config{
-		Credentials: provider,
-	}
-	if datasourceInfo.Endpoint != "" {
-		config.BaseEndpoint = aws.String(datasourceInfo.Endpoint)
-	}
-
-	backend.Logger.Debug("CreateXrayClient", "userAgent", awsds.GetUserAgentString("X-ray"))
-
-	return xray.NewFromConfig(config, func(opt *xray.Options) { opt.Region = datasourceInfo.Region }), nil
+func CreateXrayClient(ctx context.Context, settings awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings, sessions *awsds.SessionCache) (*xray.Client, error) {
+  cfg, err := getAWSConfig(ctx, settings, backendSettings)
+  if err != nil {
+    return nil, err
+  }
+  return xray.NewFromConfig(cfg), nil
 }
 
-func CreateAppSignalsClient(ctx context.Context, datasourceInfo awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings, sessions *awsds.SessionCache) (*applicationsignals.Client, error) {
-	provider, err := getXRaySession(ctx, datasourceInfo, backendSettings, sessions)
-	if err != nil {
-		return nil, err
-	}
-
-	config := aws.Config{
-		Credentials: provider,
-	}
-	if datasourceInfo.Endpoint != "" {
-		config.BaseEndpoint = aws.String(datasourceInfo.Endpoint)
-	}
-
-	backend.Logger.Debug("CreateApplicationSignalsClient", "userAgent", awsds.GetUserAgentString("X-ray"))
-	return applicationsignals.NewFromConfig(config, func(opt *applicationsignals.Options) { opt.Region = datasourceInfo.Region }), nil
+func CreateAppSignalsClient(ctx context.Context, settings awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings, sessions *awsds.SessionCache) (*applicationsignals.Client, error) {
+  cfg, err := getAWSConfig(ctx, settings, backendSettings)
+  if err != nil {
+    return nil, err
+  }
+  backend.Logger.Debug("CreateApplicationSignalsClient", "userAgent", awsds.GetUserAgentString("X-ray"))
+  return applicationsignals.NewFromConfig(cfg), nil
 }
 
-func getXRaySession(ctx context.Context, datasourceInfo awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings, sessions *awsds.SessionCache) (aws.CredentialsProvider, error) {
-	httpClientProvider := httpclient.NewProvider()
-	httpClientOptions, err := backendSettings.HTTPClientOptions(ctx)
-	if err != nil {
-		return nil, errorsource.PluginError(err, false)
-	}
+func getAWSConfig(ctx context.Context, settings awsds.AWSDatasourceSettings, backendSettings backend.DataSourceInstanceSettings) (aws.Config, error) {
+  region := settings.Region
+  if region == "" || region == "default" {
+    region = settings.DefaultRegion
+  }
+  httpClient, err := getHTTPClient(ctx, backendSettings)
+  if err != nil {
+    return aws.Config{}, err
+  }
+  return awsauth.NewConfigProvider().GetConfig(ctx, awsauth.Settings{
+    LegacyAuthType:     settings.AuthType,
+    AccessKey:          settings.AccessKey,
+    SecretKey:          settings.SecretKey,
+    Region:             region,
+    CredentialsProfile: settings.Profile,
+    AssumeRoleARN:      settings.AssumeRoleARN,
+    Endpoint:           settings.Endpoint,
+    ExternalID:         settings.ExternalID,
+    HTTPClient:         httpClient,
+  })
+}
 
-	httpClient, err := httpClientProvider.New(httpClientOptions)
-	if err != nil {
-		backend.Logger.Error("failed to create HTTP client", "error", err.Error())
-		return nil, errorsource.PluginError(err, false)
-	}
+func getHTTPClient(ctx context.Context, backendSettings backend.DataSourceInstanceSettings) (*http.Client, error) {
+  httpClientProvider := httpclient.NewProvider()
+  httpClientOptions, err := backendSettings.HTTPClientOptions(ctx)
+  if err != nil {
+    return nil, errorsource.PluginError(err, false)
+  }
 
-	provider, err := sessions.CredentialsProviderV2(ctx, awsds.GetSessionConfig{
-		Settings:      datasourceInfo,
-		HTTPClient:    httpClient,
-		UserAgentName: aws.String("X-ray"),
-	})
-	if err != nil {
-		err = errorsource.PluginError(err, false)
-	}
-	return provider, err
+  httpClient, err := httpClientProvider.New(httpClientOptions)
+  if err != nil {
+    backend.Logger.Error("failed to create HTTP client", "error", err.Error())
+    return nil, errorsource.PluginError(err, false)
+  }
+  return httpClient, nil
+
 }
